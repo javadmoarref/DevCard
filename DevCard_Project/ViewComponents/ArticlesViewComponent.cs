b5e5ed087be597cb2099c0d14a560c8f0dbc8c70
javadmoarref <javadmoorf@gmail.com>
2022-03-11 17:30:43 +0330

    project & article move into View Component

---
+++ DevCard_Project/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_Project.ViewComponents
+{
+    public class ArticlesViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1, "آموزش Asp.net core mvc", "کاملترین آموزش Asp.net core mvc به زبان فارسی",
+                    "blog-post-thumb-card-1.jpg"),
+                new Article(2, "آموزش Git & Github", "کاملترین آموزش Git & Github به زبان فارسی",
+                    "blog-post-thumb-card-2.jpg"),
+                new Article(3, "آموزش Onion", "کاملترین آموزش Onion به زبان فارسی", "blog-post-thumb-card-3.jpg"),
+                new Article(4, " آموزش طراحی سایت", "کاملترین آموزش طراحی سایت به زبان فارسی",
+                    "blog-post-thumb-card-4.jpg"),
+            };
+            return View("_Articles",articles);
+        }
+    }
+}
---
+++ DevCard_Project/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_Project.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1, "تاکسی", "درخواست آنلاین تاکسی درون شهری ","project-1.jpg", "javad"),
+                new Project(2, "زود فود", "درخواست آنلاین غذا برای کل کشور ","project-2.jpg","zodfod"),
+                new Project(3, "مدارس", "مدیریت یکپارچه مدارس  ","project-3.jpg", "monop"),
+                new Project(4, "فضا پیما", "مدیریت یکپارچه فضا پیماهای ناسا ","project-4.jpg", "nasa")
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
